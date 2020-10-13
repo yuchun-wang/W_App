ad53de757555a40294cf41341f9e6c8c055a64a6
yuchun <yuc1996229@gmail.com>
2020-10-13 19:58:48 +0800

    Initial Commit

---
+++ W_App/W_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  W_AppApp.swift
+//  W_App
+//
+//  Created by Chun on 2020/10/13.
+//
+
+import SwiftUI
+
+@main
+struct W_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
