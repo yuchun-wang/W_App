9ce6e76a528ca1c16a92b5d3043b235e8eb7f181
yuchun <yuc1996229@gmail.com>
2020-10-13 21:04:45 +0800

    EarView

--- W_App/W_AppApp.swift
+++ W_App/W_AppApp.swift
@@ -15,3 +15,9 @@ struct W_AppApp: App {
         }
     }
 }
+
+struct W_AppApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
